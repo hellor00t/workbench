0cad364f7d630ccc980f05e9e27ec1f545832e0b
hellor00t <scott@Scotts-MacBook-Pro.local>
2022-05-02 12:52:08 -0600

    update go stuff

--- kali_setup.sh
+++ kali_setup.sh
@@ -2,7 +2,7 @@
 
 ###################################################################
 #Script Name    : tool_builder.sh
-#Description    : pentesting tool management for kali/deb boxes
+#Description    : pentesting tool management for ubuntu boxes
 #Version        : .01
 #Author         : Scott
 #Twitter        : @hellor00t
@@ -12,21 +12,23 @@
 #### Functions
 
 installBasic() {
-        sudo apt install -y golang jq fzf bat
-        echo "export GOROOT=/usr/lib/go" >> ~/.zshrc
-        echo "export GOPATH=$HOME/go" >> ~/.zshrc
-        echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.zshrc
+        sudo apt install -y jq fzf bat python3-pip
+        sudo rm -rf /usr/local/go
+        sudo rm -rf /usr/bin/go
+        curl -OL https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+        sudo tar -C /usr/local -xvf go1.18.1.linux-amd64.tar.gz
+        echo "export PATH=$PATH:/usr/local/go/bin:/$HOME/go/bin" >> ~/.bashrc
         source ~/.bashrc
         # Install tomnomnom glory
         go get github.com/tomnomnom/waybackurls
-        go get go get -u github.com/ffuf/ffuf
+        go get -u github.com/ffuf/ffuf
         go get -u github.com/tomnomnom/assetfinder
-        go get https://github.com/tomnomnom/anew
+        go install -v github.com/tomnomnom/anew@latest
         go get -u github.com/tomnomnom/qsreplace
         go get -u github.com/tomnomnom/gf
         go get -u github.com/tomnomnom/meg
+        go install github.com/tomnomnom/unfurl@latest
         # Install Ferroxbuster
-        # FERROXBUSTER=$(curl -sL https://api.github.com/repos/epi052/feroxbuster/releases/latest | jq -r ".assets[].browser_download_url" | grep x86_64-linux-feroxbuster.tar.gz)
         wget -q `curl -sL https://api.github.com/repos/epi052/feroxbuster/releases/latest | jq -r ".assets[].browser_download_url" | grep x86_64-linux-feroxbuster.tar.gz`
         tar -xvf x86_64-linux-feroxbuster.tar.gz
         rm x86_64-linux-feroxbuster.tar.gz
@@ -35,7 +37,9 @@ installBasic() {
         # Clone SecLists
         git clone https://github.com/danielmiessler/SecLists ~/tools/SecLists
         go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-        npm install -g fx
+        curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+        sudo apt-get install -y nodejs
+        pip3 install updog
 }
 
 installFull() {
@@ -43,10 +47,15 @@ installFull() {
         git clone https://github.com/wallarm/jwt-secrets ~/tools/jwt-secrets
         git clone https://github.com/BBhacKing/jwt_secrets.git ~/tools/jwt_secrets
         # Install Nucleai
-        GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+        go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+        mkdir ~/nuclei-templates
         nuclei -ut
-        https://github.com/geeknik/the-nuclei-templates ~/nuclei-templates/the-nuclei-templates
-        
+        git clone https://github.com/geeknik/the-nuclei-templates ~/nuclei-templates/the-nuclei-templates
+        wget -q `curl -sL https://api.github.com/repos/assetnote/kiterunner/releases | jq -r .[0].assets[].browser_download_url | grep linux_amd64`
+        export kitepath=`curl -sL https://api.github.com/repos/assetnote/kiterunner/releases | jq -r .[0].assets[].browser_download_url | grep linux_amd64 | awk -F'/' '{print $9}'`
+        tar -xvf ${kitepath}
+        chmod 755 kr
+        sudo mv kr /usr/bin/
 }
 
 updateTools() {
